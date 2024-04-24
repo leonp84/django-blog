68600697847061da6ebcd1f8ba28c0a5c15c4e17
Leon Potgieter <hexvallei@gmail.com>
2024-04-24 15:08:01 +0000

    add allauth package and basic authentication

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', include("about.urls"), name="about_urls"),
+    path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', include("blog.urls"), name="blog_urls"),
 ]
